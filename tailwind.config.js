778373e98101b5f544394adc1b3c97ad27aef393
Shashanka Prajapati <shashankap95@gmail.com>
2021-07-04 19:14:25 +0545

    Style: Add bootstrap styles to buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+        emerald: colors.emerald,
+      },
+    },
   },
   variants: {
     extend: {},
