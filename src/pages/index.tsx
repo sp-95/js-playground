c832938338d122142535c7cb3560670559f515d5
Shashanka Prajapati <shashankap95@gmail.com>
2021-07-04 18:32:19 +0545

    Feat: Add notification buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
+import Layout from '@/components/layout'
 import Home from '@components/home'
 import React from 'react'
 
 export default function HomePage(): React.ReactElement {
-  return <Home />
+  return (
+    <Layout
+      title="Notification Component"
+      description="Simulate notification component"
+    >
+      <Home />
+    </Layout>
+  )
 }
